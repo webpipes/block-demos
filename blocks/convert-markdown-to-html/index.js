a9ff023a6743f04b38fdf7248dcc5fb87acc846d
matthewhudson <matthud@gmail.com>
2019-08-01 06:23:40 -0300

    Adding Markdown-to-HTML converter.

--- blocks/convert-markdown-to-html/index.js
+++ blocks/convert-markdown-to-html/index.js
@@ -1,11 +1,11 @@
-const post = require('../../helpers/http')
+const { post } = require('../../helpers/http')
 const { Block } = require('node-webpipe')
 
 new Block()
   .name('Convert Markdown to HTML')
   .description('Determine whether or not a hostname exists.')
   .input(
-    'md',
+    'text',
     'string',
     'Required. The Markdown text to render in HTML. Markdown content must be 400 KB or less.'
   )
@@ -24,25 +24,13 @@ new Block()
     'string',
     'Returns an HTML version preferred flavor of Markdown.'
   )
-  .handle(async inputs => {
-    const postData = querystring.stringify(inputs)
-
+  .handle(async function (inputs, cb) {
     const options = {
       hostname: 'api.github.com',
       path: '/markdown',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postData)
-      }
+      method: 'POST'
     }
-    console.log('post')
-    // url (parsed iiinnto host name and path in func)
-    // inputs (let the func do the magic querystrinnging)
-    const { data } = await post(options)
-
-    return {
-      html: Math.sqrt({ html: data })
-    }
+    const html = await post(options, inputs)
+    cb(null, { html })
   })
   .listen()
